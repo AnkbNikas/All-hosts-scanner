845a7cfcfeda6838d77566421e340d7dff51e3b7
AnkbNikas <nieveskaskero@hotmail.com>
2024-11-11 10:48:56 +0100

    Create all_hosts_scanner.py

---
+++ all_hosts_scanner.py
@@ -0,0 +1,13 @@
+import nmap
+
+def scan_all_hosts(network):
+    nm = nmap.PortScanner()
+    print(f"Scanning network: {network}")
+    nm.scan(hosts=network, arguments='-sn')  # -sn para hacer un escaneo de ping
+    hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
+    for host, status in hosts_list:
+        print(f"Host: {host} is {status}")
+
+if __name__ == "__main__":
+    network = input("Enter the network to scan (e.g., 192.168.1.0/24): ")
+    scan_all_hosts(network)
